741df25f4c4a70e6d3464076d39b1d91389c42cc
malakamadushan <malakamadushan123@gmail.com>
2019-02-20 15:47:25 +0530

    1st implementation

---
+++ src/lk/ijse/dep/app/util/AppConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep.app.util;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("lk.ijse.dep.app")
+public class AppConfig {
+}
